c3059c94fa7ad5429c0d6de0ae033f86d190b994
Philip I. Thomas <mail@philipithomas.com>
2014-11-09 18:33:49 -0800

    string comparison

--- site/build.sh
+++ site/build.sh
@@ -8,11 +8,11 @@ bundle exec jekyll build
 # If we're in travis and on master branch - Deploy all the things!
 # (we treat deployment as a test)
 
-if [ "$TRAVIS" == "true" ] \
-    && [ "$TRAVIS_BRANCH" == "master" ] \
-    && [ "$TRAVIS_PULL_REQUEST" == "false" ] \
-    && [ "$TRAVIS_REPO_SLUG" == "philipithomas/TuringMechanic" ] \
-    && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]
+if [ "$TRAVIS" = "true" ] \
+    && [ "$TRAVIS_BRANCH" = "master" ] \
+    && [ "$TRAVIS_PULL_REQUEST" = "false" ] \
+    && [ "$TRAVIS_REPO_SLUG" = "philipithomas/TuringMechanic" ] \
+    && [ "$TRAVIS_SECURE_ENV_VARS" = "true" ]
 then
       # Push the build using the s3_website.yml settings
       bundle exec s3_website push
