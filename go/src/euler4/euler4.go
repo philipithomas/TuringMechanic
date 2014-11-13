f1e4f4d141b631d6b18bfe12f5aab66f2b2be939
Philip I. Thomas <mail@philipithomas.com>
2014-11-12 23:17:53 -0800

    Get full function working, and add test for final answer

--- go/src/euler4/euler4.go
+++ go/src/euler4/euler4.go
@@ -3,6 +3,12 @@ package main
 import (
     "fmt"
     "strconv"
+    "errors"
+)
+
+var (
+    lower = 100
+    upper = 999
 )
 
 func isPalindrome(n int) bool {
@@ -16,5 +22,27 @@ func isPalindrome(n int) bool {
     return true
 }
 
+func calculate() (max int, err error){
+    for i:=lower; i<=upper; i++ {
+        for j:=lower;j<=upper;j++ {
+            test := i*j;
+            if isPalindrome(test) {
+                if max < test {
+                    max = test
+                }
+            }
+        }
+    }
+    if max == 0 {
+        err = errors.New("No value found")
+    }
+    return
+}
+
 func main() {
+    max, err := calculate()
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("The max palindrome formed by the multplication of two three-digit numbers is %d\n", max)
 }
--- go/src/euler4/euler4_test.go
+++ go/src/euler4/euler4_test.go
@@ -4,6 +4,18 @@ import (
     "testing"
 )
 
+func TestCorrectEulerAnswer(t *testing.T) {
+    result, err := calculate()
+    if err != nil {
+        t.Error("Error thrown by calcualte")
+    }
+
+    solution := 906609
+    if result != solution {
+        t.Errorf("Solution was incorrect. Correct: %d. Calculated: %d.", result, solution)
+    }
+}
+
 func TestIntegralPalindromesArePalindromes(t *testing.T) {
     palindromes := [...]int{11, 101, 111, 1221, 12321, 1234321}
     for _, n := range palindromes {
